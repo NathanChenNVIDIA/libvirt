155b4909f14d624d8d8b54f4a8474e602355c1a1
Nathan Chen <nathanc@nvidia.com>
2024-10-30 23:28:13 +0000

    conf: Add a smmuv3Accel IOMMU model
    Add support for specifying "smmuv3Accel" as the IOMMU model. In the following
    commits, when the "smmuv3Accel" IOMMU model is parsed from the VM definition,
    PXB controllers each associated with a SMMU node will be auto-added to the VM
    definition, and VFIO devices will be routed to PXB controllers based on their
    association with host SMMU nodes. VFIO devices with unique host SMMU node
    associations will be attached to unique PXB controllers.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -6176,6 +6176,9 @@ qemuBuildIOMMUCommandLine(virCommand *cmd,
         /* There is no -device for SMMUv3, so nothing to be done here */
         return 0;
 
+    case VIR_DOMAIN_IOMMU_MODEL_SMMUV3_ACCEL:
+        return 0;
+
     case VIR_DOMAIN_IOMMU_MODEL_LAST:
     default:
         virReportEnumRangeError(virDomainIOMMUModel, iommu->model);
@@ -7009,7 +7012,8 @@ qemuBuildMachineCommandLine(virCommand *cmd,
         case VIR_DOMAIN_IOMMU_MODEL_VIRTIO:
             /* These IOMMUs are formatted in qemuBuildIOMMUCommandLine */
             break;
-
+        case VIR_DOMAIN_IOMMU_MODEL_SMMUV3_ACCEL:
+            break;
         case VIR_DOMAIN_IOMMU_MODEL_LAST:
         default:
             virReportEnumRangeError(virDomainIOMMUModel, def->iommu->model);
