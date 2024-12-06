a072956f029a9a131a67c8cda32e5110f02ac5c2
Nathan Chen <nathanc@nvidia.com>
2024-12-06 05:14:50 +0000

    qemu: Add test case for specifying multiple nested SMMUs
    Implement a sub-test in qemuxmlconftest that mocks the scanning of sysfs to
    determine how many "nestedSmmuv3" devices to assign to the VM when
    "nestedSmmuv3" IOMMU model is parsed from the VM definition. Add a VM
    definition example with an associated qemu command line that exercises the
    detection of "nestedSmmuv3" IOMMU model and auto-creation of PXB controllers and
    "nestedSmmuv3" devices.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -2758,6 +2758,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST_ABI_UPDATE("intel-iommu-eim-autoadd");
     DO_TEST_CAPS_LATEST_ABI_UPDATE("intel-iommu-eim-autoadd-v2");
     DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3", "aarch64");
+    DO_TEST_CAPS_ARCH_LATEST("iommu-nestedsmmuv3", "aarch64");
     DO_TEST_CAPS_LATEST("virtio-iommu-x86_64");
     DO_TEST_CAPS_ARCH_LATEST("virtio-iommu-aarch64", "aarch64");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-wrong-machine");
@@ -3020,7 +3021,8 @@ VIR_TEST_MAIN_PRELOAD(mymain,
                       VIR_TEST_MOCK("virrandom"),
                       VIR_TEST_MOCK("qemucpu"),
                       VIR_TEST_MOCK("virpci"),
-                      VIR_TEST_MOCK("virnuma"))
+                      VIR_TEST_MOCK("virnuma"),
+                      VIR_TEST_MOCK("virnestedsmmuv3"))
 
 #else
 
