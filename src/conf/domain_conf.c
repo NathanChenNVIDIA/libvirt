93447192362026899a9e51aca971341fa89aeff6
Nathan Chen <nathanc@nvidia.com>
2024-12-10 03:55:31 +0000

    qemu: Implement support for associating iommufd to hostdev
    Implement "iommufdId" and "iommufdFd" attributes for "hostdev" devices that
    can be used to specify associated iommufd object and fd for externally opening
    the /dev/iommu and VFIO cdev, when launching a qemu VM.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -13357,6 +13357,21 @@ virDomainVideoDefParseXML(virDomainXMLOption *xmlopt,
     return g_steal_pointer(&def);
 }
 
+static void
+virDomainHostdevDefIommufdParseXML(xmlXPathContextPtr ctxt,
+                                   char** iommufdId,
+                                   char** iommufdFd)
+{
+    g_autofree char *iommufdIdtmp = virXPathString("string(./iommufdId)", ctxt);
+    g_autofree char *iommufdFdtmp = virXPathString("string(./iommufdFd)", ctxt);
+
+    if (iommufdIdtmp)
+        *iommufdId = g_steal_pointer(&iommufdIdtmp);
+
+    if (iommufdFdtmp)
+        *iommufdFd = g_steal_pointer(&iommufdFdtmp);
+}
+
 static virDomainHostdevDef *
 virDomainHostdevDefParseXML(virDomainXMLOption *xmlopt,
                             xmlNodePtr node,
@@ -13431,6 +13446,8 @@ virDomainHostdevDefParseXML(virDomainXMLOption *xmlopt,
 
     if (virDomainNetTeamingInfoParseXML(ctxt, &def->teaming) < 0)
         goto error;
+
+    virDomainHostdevDefIommufdParseXML(ctxt, &def->iommufdId, &def->iommufdFd);
 
     return def;
 
@@ -20847,6 +20864,16 @@ virDomainHostdevDefCheckABIStability(virDomainHostdevDef *src,
         return false;
     }
 
+    if (src->iommufdId && dst->iommufdId) {
+        if (STRNEQ(src->iommufdId, dst->iommufdId))
+            return false;
+    }
+
+    if (src->iommufdFd && dst->iommufdFd) {
+        if (STRNEQ(src->iommufdFd, dst->iommufdFd))
+            return false;
+    }
+
     if (!virDomainDeviceInfoCheckABIStability(src->info, dst->info))
         return false;
 
@@ -27086,6 +27113,11 @@ virDomainHostdevDefFormat(virBuffer *buf,
         virBufferAddLit(buf, "<readonly/>\n");
     if (def->shareable)
         virBufferAddLit(buf, "<shareable/>\n");
+    if (def->iommufdId) {
+        virBufferAsprintf(buf, "<iommufdId>%s</iommufdId>\n", def->iommufdId);
+        if (def->iommufdFd)
+            virBufferAsprintf(buf, "<iommufdFd>%s</iommufdFd>\n", def->iommufdFd);
+    }
 
     virDomainDeviceInfoFormat(buf, def->info, flags | VIR_DOMAIN_DEF_FORMAT_ALLOW_BOOT
                                                     | VIR_DOMAIN_DEF_FORMAT_ALLOW_ROM);
--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -389,6 +389,8 @@ struct _virDomainHostdevDef {
         virDomainHostdevCaps caps;
     } source;
     virDomainNetTeamingInfo *teaming;
+    char *iommufdId;
+    char *iommufdFd;
     virDomainDeviceInfo *info; /* Guest address */
 };
 
