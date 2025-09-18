4188e85cfe2f8b75b6d5bac1a3d3001ef320252c
Nathan Chen <nathanc@nvidia.com>
2025-09-18 02:47:41 +0000

    tests: qemuxmlconfdata: provide smmuv3Dev sample XML and CLI args
    Provide sample XML and CLI args for the smmuv3Dev XML schema
    for virt machine type.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -2964,6 +2964,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST_ABI_UPDATE("intel-iommu-eim-autoadd");
     DO_TEST_CAPS_LATEST_ABI_UPDATE("intel-iommu-eim-autoadd-v2");
     DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3", "aarch64");
+    DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3Dev", "aarch64");
     DO_TEST_CAPS_LATEST("virtio-iommu-x86_64");
     DO_TEST_CAPS_ARCH_LATEST("virtio-iommu-aarch64", "aarch64");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-wrong-machine");
