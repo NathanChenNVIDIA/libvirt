8a584c235cdbda0104e25f254fb7701f2fffc77f
Nathan Chen <nathanc@nvidia.com>
2025-10-28 20:07:59 +0000

    conf: Support EGM memory device model
    Add support for EGM memory device model with
    'path' source attribute and 'pciDev' target
    attribute to denote host EGM device backing path
    and PCI device alias to associate the vEGM with,
    respectively.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -2743,6 +2743,7 @@ typedef enum {
     VIR_DOMAIN_MEMORY_MODEL_VIRTIO_PMEM, /* virtio-pmem memory device */
     VIR_DOMAIN_MEMORY_MODEL_VIRTIO_MEM, /* virtio-mem memory device */
     VIR_DOMAIN_MEMORY_MODEL_SGX_EPC, /* SGX enclave page cache */
+    VIR_DOMAIN_MEMORY_MODEL_EGM, /* Extended GPU memory */
 
     VIR_DOMAIN_MEMORY_MODEL_LAST
 } virDomainMemoryModel;
@@ -2775,6 +2776,9 @@ struct _virDomainMemoryDef {
         struct {
             virBitmap *nodes; /* source NUMA nodes */
         } sgx_epc;
+        struct {
+            char *path;
+        } egm;
     } source;
 
     union {
@@ -2800,6 +2804,9 @@ struct _virDomainMemoryDef {
         } virtio_mem;
         struct {
         } sgx_epc;
+        struct {
+            char *pciDev;
+        } egm;
     } target;
 
     virDomainDeviceInfo info;
