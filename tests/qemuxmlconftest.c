06f07340b49ae33cefaeb69c2131a764591c83d0
Nathan Chen <nathanc@nvidia.com>
2025-10-29 20:29:02 +0000

    tests: qemuxmlconfdata: provide cmdqv sample XML and CLI args
    Provide sample XML and CLI args for the cmdqv XML schema
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -3042,6 +3042,7 @@ mymain(void)
     DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3-pci-bus", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3-pci-bus-single", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3-pci-bus-accel", "aarch64");
+    DO_TEST_CAPS_ARCH_LATEST("iommu-smmuv3-pci-bus-accel-cmdqv", "aarch64");
     DO_TEST_CAPS_LATEST("virtio-iommu-x86_64");
     DO_TEST_CAPS_ARCH_LATEST("virtio-iommu-aarch64", "aarch64");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-wrong-machine");
